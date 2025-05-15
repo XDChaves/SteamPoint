861a0e40c9fc1681169a4b6a978dfb26c89550a8
XDChaves <hpchavesxdr@gmail.com>
2025-05-15 13:06:41 -0300

    Add review endpoint and implement pagination for tags

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 import pandas as pd
+import math
 
 app = Flask(__name__)
 CORS(app)
 
-
+tags_csv = 'DataCSV/tags.csv'
 games_csv = 'DataCSV/games.csv'
 steamspy_csv = 'DataCSV/steamspy_insights.csv'
 reviews_csv = 'DataCSV/review.csv'
-promotional_csv = 'DataCSV/promotional.csv'
 categories_csv = 'DataCSV/categories.csv'
-tags_csv = 'DataCSV/tags.csv'
-
+promotional_csv = 'DataCSV/promotional.csv'
 
 @app.route('/steam-games')
 def steam_games():
@@ -94,6 +93,20 @@ def steam_categories():
 
     return json_data_cat
 
+@app.route('/review')
+def steam_reviews():
+    games= pd.read_csv(games_csv, sep=',')
+    dg_filter = games[["app_id", "name"]]
+
+    drev = pd.read_csv('DataCSV/review.csv', sep=',')
+    filtered = drev[["app_id","review_score","review_score_description","positive","negative","total","metacritic_score","recommendations"]]
+
+    df = pd.merge(dg_filter, filtered, on='app_id', how='inner')
+
+    json_data_tag = df.to_json(orient='table', indent=3, force_ascii=False)
+
+    return json_data_tag
+
 @app.route('/tags')
 def steam_tags():
     games= pd.read_csv(games_csv, sep=',')
@@ -108,6 +121,48 @@ def steam_tags():
 
     return json_data_tag
 
+
+# testando paginação
+def get_total_and_filtered_data(search_value=None):
+    games = pd.read_csv(games_csv, sep=',')
+    dg_filter = games[["app_id", "name"]]
+    dtag = pd.read_csv(tags_csv, sep=',')
+    df = pd.merge(dg_filter, dtag, on='app_id', how='inner')
+
+    if search_value:
+        df_filtered = df[df['name'].str.contains(search_value, case=False) | df['tag'].str.contains(search_value, case=False)]
+        return len(df), len(df_filtered), df_filtered
+    else:
+        return len(df), len(df), df
+
+@app.route('/tags-modify')
+def steam_tags_teste():
+    draw = int(request.args.get('draw', 1))
+    start = int(request.args.get('start', 0))
+    length = int(request.args.get('length', 10))
+    search_value = request.args.get('search[value]', '')
+    order_column_index = int(request.args.get('order[0][column]', 0))
+    order_direction = request.args.get('order[0][dir]', 'asc')
+    columns = ['app_id', 'name', 'tag']
+    order_column = columns[order_column_index]
+
+    total_records, filtered_records, df_filtered = get_total_and_filtered_data(search_value)
+
+    # Ordenar os dados filtrados
+    df_filtered = df_filtered.sort_values(by=order_column, ascending=(order_direction == 'asc'))
+
+    # Paginar os dados filtrados
+    df_paginated = df_filtered[start : start + length]
+
+    data = df_paginated[['app_id', 'name', 'tag']].to_dict('records')
+
+    response = {
+        "draw": draw,
+        "recordsTotal": total_records,
+        "recordsFiltered": filtered_records,
+        "data": data
+    }
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
